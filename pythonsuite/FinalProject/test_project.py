a807e4b6f6c70c244400f1dfcda8a6f9da34d1a6
Ivan Gorshenin (REMOTE) <anfield_road@mail.ru>
2022-11-09 23:05:35 +0300

    Unit Test are done

--- pythonsuite/FinalProject/test_project.py
+++ pythonsuite/FinalProject/test_project.py
@@ -1,25 +1,25 @@
-def test_function_1():
-    pass
+from project import is_alphabet, get_string_by_number, get_cipher_key, prepare_cipher_alphabet
 
 
-def test_function_2():
-    pass
+def test_is_alphabet():
+    assert is_alphabet("Ivan") == True
+    assert is_alphabet("1234") == False
+    assert is_alphabet("A B") == True
+    assert is_alphabet("A.") == False
 
 
-def test_function_n():
-    pass
+def test_get_string_by_number():
+    assert get_string_by_number(0) == "nil"
+    assert get_string_by_number(12) == "twelve"
+    assert get_string_by_number(18) == "eighteen"
+    assert get_string_by_number(59) == "fiftynine"
 
 
+def test_get_cipher_key():
+    assert get_cipher_key("01/01/2022, 00:00:00") == "nilnilnilonesaturdayjanuary"
+    assert get_cipher_key("31/12/2022, 23:59:59") == "fiftyninefiftyninetwentythreethirtyonesaturdaydecember"
 
-#from lecture5_unittests import square
- 
-#def test_positive():
-#    assert square(2) == 4
-#    assert square(3) == 9
 
-#def test_negative():
-#    assert square(-2) == 4
-#    assert square(-3) == 9
-
-#def test_zero():
-#    assert square(0) == 0
+def test_prepare_cipher_alphabet():
+    assert prepare_cipher_alphabet("01/01/2022, 00:00:00") == "niloesaturdyjbcfghkmpqvwxz"
+    assert prepare_cipher_alphabet("31/12/2022, 23:59:59") == "fitynewhrosaudcmbgjklpqvxz"
